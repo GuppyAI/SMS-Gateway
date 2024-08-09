25d345304d21a2365ba18bf5f0ff38c022ca56dd
Lucca Greschner <luccagreschner@gmail.com>
2024-08-09 22:19:32 +0200

    Add logging for sending message to service bus

--- internal/servicebus/message_handler.go
+++ internal/servicebus/message_handler.go
@@ -2,6 +2,7 @@ package servicebus
 
 import (
 	"context"
+	"github.com/rs/zerolog/log"
 	"sms-gateway/internal/messaging"
 )
 
@@ -14,6 +15,11 @@ func NewMessageHandler(sender Sender) (*MessageHandler, error) {
 }
 
 func (m MessageHandler) Handle(message messaging.Message) error {
+	log.Debug().
+		Str("address", message.GetAddress().String()).
+		Str("message", message.GetMessageBody()).
+		Msg("Sending message to service bus!")
+
 	err := m.sender.SendMessage(context.TODO(), toServiceBusMessage(message), nil)
 	if err != nil {
 		return err
